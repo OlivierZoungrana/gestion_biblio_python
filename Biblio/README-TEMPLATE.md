fb2aceff9170b53163474b8aba491ef998e5c08e
OlivierZoungrana <synthejazz@gmail.com>
2022-09-13 17:09:25 +0200

    ajout

---
+++ Biblio/README-TEMPLATE.md
@@ -0,0 +1,5 @@
+# Hey! Nice to see you
+
+Welcome to my page!
+I'm my_name, a geek from Paris..
+
---
+++ Biblio/build.py
@@ -0,0 +1,7 @@
+from profile_readme import get_github_context, ProfileGenerator
+
+
+if __name__ == "__main__":
+    context = {}
+    # If you don't need the GitHub data you can remove the next line
+
